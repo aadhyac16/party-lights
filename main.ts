8948fe4c77f6af011f3a9465bb01b5a500f2109b
aadhyac16 <83731774+aadhyac16@users.noreply.github.com>
2021-06-11 13:25:57 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+    led.setBrightness(input.soundLevel())
+})
